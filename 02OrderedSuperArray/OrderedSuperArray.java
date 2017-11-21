5ca62c7f2f7f2ca3cf592d4e3472fb58ef62f7aa
William <wlohier@stuy.edu>
2017-11-21 15:15:58 -0400

    Added 02OrderedSuperArray directory and files

---
+++ 02OrderedSuperArray/OrderedSuperArray.java
@@ -0,0 +1,89 @@
+import java.util.*;
+
+public class OrderedSuperArray extends SuperArray{
+    
+    public OrderedSuperArray(){
+	this(10);
+    }
+
+    public OrderedSuperArray(int capacity){
+	super(capacity);
+    }
+
+    public OrderedSuperArray(String[] ary){
+        this(ary.length);
+	for(int i = 0; i < ary.length; i++){
+	    add(ary[i]);
+	}
+    }
+    
+    public void add(int index, String value){
+    add(value);
+    }
+
+    public String set(int index, String element){
+	throw new UnsupportedOperationException();
+    }
+
+
+    public boolean add(String value){
+	int index = findIndex(value);
+        super.add(index, value);
+        return true;
+    }
+
+
+
+    /*  public int findIndex2(String str){
+	if(size < 1){
+	    return 0;
+	}
+	int start = 0;
+	int end = size;
+	int middle = end/2;
+	while (start != end) {
+	    System.out.println("middle = " + middle);
+	    System.out.println(toString());
+	    System.out.println("data[middle] = " + data[1]);
+	    if(str.compareTo(data[middle]) < 0){
+		end = middle;
+		middle = (middle + 1)/2;
+	    } else if(str.compareTo(data[middle]) > 0){
+		start = middle;
+		middle = middle + middle/2;
+	    } else {
+		return middle;
+	    }
+	}
+	return middle;
+	} */
+
+    public int findIndex(String str){
+	for(int i = 0; i < size(); i++){
+	    if(str.compareTo(get(i)) < 1){
+		return i;
+	    }
+	}
+	return size();
+    }
+
+
+    public int indexOf(String str){
+	for(int i = findIndex(str); i >= 0; i--){
+	    System.out.println("i = " + i);
+	    if(str.compareTo(get(i)) > 0){
+		return i + 1;
+	    }
+	}
+	return -1;
+    }
+
+    public int lastIndexOf(String str){
+	for(int i = findIndex(str); i <= size(); i++){
+	    if(str.compareTo(get(i)) < 0){
+		return i - 1;
+	    }
+	}
+	return -1;
+    }
+}
---
+++ 02OrderedSuperArray/SuperArray.java
@@ -0,0 +1,133 @@
+public class SuperArray{
+    private String[] data;
+    private int size;
+
+    public SuperArray(int len){
+    data = new String[len];
+    size = 0;
+    } public SuperArray(){
+    this(10);
+    }
+    
+    public void clear(){
+    data = new String[data.length];
+    size = 0;
+    }
+
+    public int size(){
+    return size;
+    }
+
+    public boolean add(String element){
+	if(data.length >= size - 1){
+	    resize();
+	    System.out.println("had to resize");
+	}
+	data[size] = element;
+        size += 1;
+	return true;
+    }
+
+    public String get(int index){
+	if(index > -1 && index < size()){
+	    return data[index];
+	}else{
+	    System.out.println("ERROR - index out of range for get");
+	    return null;
+	}
+    }
+
+    public String set(int index, String element){
+	if(index >= 0 && index < data.length){
+	    String old = data[index];
+	    data[index] = element;
+	    return old;
+	} else {
+	    System.out.println("ERROR - index out of range for set");
+	    return null;
+	}
+    }
+
+    public String toString(){
+    String p = "[";
+    for(int i = 0; i < size; i++){
+        p += data[i] + ",";
+    }
+    p = p.substring(0, p.length() - 1);
+    p += "]";
+    return p;
+    }
+
+    private void resize(){
+	String[] larger = new String[2 * data.length + 1];
+	for(int i = 0; i < size; i++){
+	    larger[i] = data[i];
+	} data = larger;
+    }
+
+    public boolean contains(String element){
+	for(int i = 0; i < size; i++){
+	    if(data[i].equals(element)){
+		return true;    
+	    }
+	}System.out.println("Element not in array");
+	return false;
+    }
+
+    public int indexOf(String element){
+	for(int i = 0; i < size; i++){
+	    if(data[i].equals(element)){
+		return i;
+	    }
+	} System.out.println("element not in array");
+	return -1;
+    }
+
+    public int lastIndexOf(String element){
+	for(int i = size - 1; i > 0; i--){
+	    if(data[i].equals(element)){
+		return i;
+	    }
+	} System.out.println("element not in array");
+	return -1;
+    }
+
+    public void add(int index, String element){
+	if(index > data.length|| index < 0){
+	    System.out.println("add index out of range");
+	}else{
+	    if(size >= data.length - 1){
+		System.out.println("Need to resize");
+		resize();
+	    } for(int i = size + 1; i > index; i--){
+		data[i] = data[i - 1];
+	    } data[index] = element;
+	    size += 1;
+	}
+    }
+
+    public String remove(int index){
+	if(index > size - 1 || index < 0){
+	    System.out.println("remove index out of range");
+	    return null;
+	}else{
+	    String val = data[index];
+	    for(int i = index; i < size - 2; i++){
+		data[i] = data[i + 1];
+		data[size - 1] = null;
+	    } size -= 1;
+	    return val;
+	}
+    }
+
+    public String remove(String element){
+	if(!contains(element)){
+	    System.out.println("Error - element not in Array");
+	    return null;
+	}else{
+	    System.out.println("remove element in array");
+	    return remove(indexOf(element));
+	}
+    }
+  
+}
