d139d14ba5047e6ca7c21bb27275aab7e688466c
William Lohier <wlohier@stuy.edu>
2017-11-28 15:47:02 -0500

    Added Constructors for Book.java

---
+++ 05Library/Book.java
@@ -0,0 +1,18 @@
+public class Book{
+    public String author;
+    public String title;
+    public String ISBN;
+
+    public Book(){
+    }
+
+    public Book(String a, String t, String I){
+	author = a;
+	title = t;
+	ISBN = i;
+    }
+
+
+
+    
+}
