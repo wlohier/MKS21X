d80489cea75157756671227f3b01b9e4a1fb8df8
William <wlohier@stuy.edu>
2017-12-04 19:59:56 -0400

    Added toString method to barcode

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -6,11 +6,11 @@ public class Barcode{
 								"|::|:", "|:|::"};
 	
 	public Barcode(String z){
-		if(zip.length() != 5){
+		if(zip.length() != 5){ //makes sure the length is correct
 			throw new IllegalArgumentException();
 		}
 		
-		for(int i = 0; i < 5; i++){
+		for(int i = 0; i < 5; i++){ //makes sure all characters are digits
 			if(!Character.isDigit(zip.charAt(i))){
 				throw new IllegalArgumentException();
 			}
@@ -19,4 +19,39 @@ public class Barcode{
 		zip = z;	
 	}
 	
+	public int calcCheck(){ //calculates the check number
+		int sum = 0;
+		for(int i = 0; i < 5; i++){
+			sum += zip.charAt(i) - '0';
+		}
+		sum = sum % 10;
+		return sum;
+	}
+	
+	public String genCode(){ //generates the barcode
+		String ans = "|";
+		for(int i = 0; i < 5; i++){
+			ans += codes[zip.charAt(i) - '0'];
+		}
+		ans += codes[calcCheck()];
+		ans += "|";
+		return ans;
+	}
+	
+	public String toString(){
+		String ans = genCode() + " ";
+		ans += "(" + zip + ")";
+		return ans;
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
