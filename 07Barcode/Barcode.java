cbe5b7c42d5888d6c287a1e971b652ad317dce5b
William <wlohier@stuy.edu>
2017-12-04 20:16:44 -0400

    added static toCode method, and calcCheck to calculate check number

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -18,27 +18,27 @@ public class Barcode implements Comparable<Barcode>{
 		zip = z;	
 	}
 	
-	private int calcCheck(){ //calculates the check number
+	private int calcCheck(String z){ //calculates the check number
 		int sum = 0;
 		for(int i = 0; i < 5; i++){
-			sum += zip.charAt(i) - '0';
+			sum += z.charAt(i) - '0';
 		}
 		sum = sum % 10;
 		return sum;
 	}
 	
-	private String genCode(){ //generates the barcode
+	private String toCode(String z){ //generates the barcode given a zip
 		String ans = "|";
 		for(int i = 0; i < 5; i++){
-			ans += codes[zip.charAt(i) - '0'];
+			ans += codes[z.charAt(i) - '0'];
 		}
-		ans += codes[calcCheck()];
+		ans += codes[calcCheck(z)];
 		ans += "|";
 		return ans;
 	}
 	
 	public String toString(){
-		String ans = genCode() + " ";
+		String ans = toCode(zip) + " ";
 		ans += "(" + zip + ")";
 		return ans;
 	}
@@ -50,6 +50,12 @@ public class Barcode implements Comparable<Barcode>{
 	public String getZip(){
 		return zip;
 	}
+	
+	public String getCode(){
+		return toCode(zip);
+	}
+		
+	
 }
 
 
