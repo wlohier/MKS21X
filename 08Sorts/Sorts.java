8eac115dbd8610f8c12fa4ac889f41285a35397e
William <wlohier@stuy.edu>
2017-12-06 21:29:01 -0400

    wrote main and selection sort

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -1,4 +1,4 @@
-import java.util.arrays;
+import java.util.Arrays;
 public class Sorts{
 	
 	public static String name(){
@@ -14,11 +14,40 @@ public class Sorts{
 		return true;
 	}
 	
+	public static void swap(int[] ary, int a, int b){ //swaps two ints in an array given their indexes
+		int temp = ary[a];
+		ary[a] = ary[b];
+		ary[b] = temp;
+	}
+	
 	public static void insertionSort(int[] ary){
 		
 	}
 	
 	public static void selectionSort(int[] ary){
-		
+		if(!isSorted(ary)){
+			for(int l = 0; l < ary.length; l++){
+				int sm = l; //index of the smallest element in the array
+				for(int i = l; i < ary.length; i++){
+					if(ary[i] < ary[sm]){
+						sm = i;
+					}
+				}
+				swap(ary, l, sm);
+			}
+		}
 	}
+	
+	public static void main(String[] args){
+		int[] beyonce = new int[15];
+		for(int i = 0; i < beyonce.length; i++){
+			beyonce[i] = (int)(Math.random()*beyonce.length);
+		}
+		
+		System.out.println(Arrays.toString(beyonce));
+		selectionSort(beyonce);
+		System.out.println(Arrays.toString(beyonce));
+	}
+	
+	
 }
